f927527284506757c0ed8a83f7c7c0e964695ffa
dyrewolf <stryder6044@gmail.com>
2018-04-30 14:06:31 -0700

    initial change test
    Added line to script

---
+++ Assets/Checking123.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Microsoft is a big poop");
+		print("first dumb change");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
